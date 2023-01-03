8fa2a41da81f7d2be54fe7c03464237a92f80bf6
jerhum <jerhum@msn.com>
2023-01-03 13:22:31 +0100

    add src

---
+++ src/components/TVShowListItem/TVShowListItem.jsx
@@ -0,0 +1,9 @@
+import { SMALL_IMG_COVER_BASE_URL } from "../../config";
+import s from "./style.module.css";
+
+export function TVShowListItem({ tvShow, onClick }) {
+  return <div className={s.container} onClick={() => onClick(tvShow)}>
+    <img className={s.img} src={SMALL_IMG_COVER_BASE_URL+ tvShow.backdrop_path} alt={tvShow.name} />
+    <div className={s.title}>{tvShow.name}</div>
+  </div>;
+}
